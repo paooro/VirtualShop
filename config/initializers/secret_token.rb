28728fe7be4ace6710036936cd77858f9f20e0ff
STCAV <stcav@stcav-Vostro-430.(none)>
2013-12-15 11:34:57 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VirtualShop::Application.config.secret_token = '90ad4c355935dddcd0e2d3a8c992dfacf4068fbb923bdc88c590719721028e55cd008467837aa6dec71aafe9907357d2fd1d715ff09b73ee8a0e6a40a66f3642'
